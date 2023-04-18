dff67a53496eef12919d656fe296b36b13fab62a
Kunal-inno <sakshi_gupta@innotechteam.in>
2023-04-18 17:01:50 +0530

    vueexercisetwo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <MyFirstModal modalTitle="Sign Up for the GiveAway" modalContent="Grab Your ninja swag for half price">
+    <template #links>
+      <a href="#" class="text-white rounded bg-green-500 px-4 py-2 font-sans font-semibold shadow-lg hover:bg-green-400 outline-none active:outline-none">Green Button</a>
+      <a href="#" class="text-white rounded bg-red-500 px-4 py-2 font-sans font-semibold shadow-lg hover:bg-red-400 outline-none active:outline-none">Red Button</a>
+    </template>
+  </MyFirstModal>
+
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyFirstModal from './components/MyFirstModal.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+components: {
+MyFirstModal
+},
+};
 </script>
+
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+font-family: Avenir, Helvetica, Arial, sans-serif;
+-webkit-font-smoothing: antialiased;
+-moz-osx-font-smoothing: grayscale;
+text-align: center;
+color: #2c3e50;
+margin-top: 60px;
 }
 </style>
---
+++ src/components/MyFirstModal.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+    <button class="text-white rounded bg-pink-500 px-6 py-4 font-sans font-semibold shadow-lg hover:bg-pink-400 outline-none active:outline-none" @click="toggleModal = true">Open Modal</button>
+    <div class="fixed overflow-x-hidden overflow-y-auto inset-0 flex justify-center items-center z-50" v-if="toggleModal">
+        <div class="absolute inset-0 bg-black opacity-25" @click.self="toggleModal = false"></div>
+        <div class="relative mx-auto w-auto max-w-2xl">
+            <div class="bg-white rounded shadow-xl w-96 h-56 flex flex-col">
+                <div class="text-sky-500 text-2xl font-bold px-4 py-6">{{ modalTitle }}</div>
+                <span>{{ modalContent }}</span>
+                <div class="mt-4">
+                    <slot name="links">
+                    </slot>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+    
+<script>
+export default {
+    name: "MyfirstModal",
+    props: {
+        modalTitle: {
+            type: String,
+            required: true
+        },
+        modalContent: {
+            type: String,
+            required: true
+        }
+    },
+    data() {
+        return {
+            toggleModal: false
+        };
+    }
+};
+</script>
