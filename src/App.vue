d7199b504a64b692c157a146ca17a070f52458e7
Kunal-inno <sakshi_gupta@innotechteam.in>
2023-04-19 15:34:55 +0530

    exechanges

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-<input :value="text"  ref="myInput" />
+<input :value="text" ref="myInput" />
 <button @click="focusOnInput">Focus</button>
+<button class="text-white rounded bg-pink-500 px-6 py-4 font-sans font-semibold shadow-lg hover:bg-pink-400 outline-none active:outline-none mt-5" @click="toggleModal = !toggleModal">Open Modal</button>
 
-<MyFirstModal modalTitle="Sign Up for the GiveAway" modalContent="Grab Your ninja swag for half price">
-    <template #links>
-        <a href="#" class="text-white rounded bg-green-500 px-4 py-2 font-sans font-semibold shadow-lg hover:bg-green-400 outline-none active:outline-none">Green Button</a>
-        <a href="#" class="text-white rounded bg-red-500 px-4 py-2 font-sans font-semibold shadow-lg hover:bg-red-400 outline-none active:outline-none">Red Button</a>
-    </template>
+<MyFirstModal modalTitle="Sign Up for the GiveAway" modalContent="Grab Your ninja swag for half price" :show-modal="toggleModal" @update:showModal = "toggleModal = $event">
 </MyFirstModal>
 </template>
 
+  
+  
 <script>
 import MyFirstModal from './components/MyFirstModal.vue';
 
@@ -17,15 +16,16 @@ export default {
     components: {
         MyFirstModal
     },
-    methods : {
-      focusOnInput() {
-        this.$refs.myInput.focus();
-      }
+    methods: {
+        focusOnInput() {
+            this.$refs.myInput.focus();
+        }
     },
     data() {
-      return {
-        message : ''
-      }
+        return {
+            message: '',
+            toggleModal: false
+        }
     }
 };
 </script>
@@ -40,13 +40,13 @@ export default {
     margin-top: 60px;
 }
 
-input{
-  border: 1px solid grey;
-  margin-top: 1%;
+input {
+    border: 1px solid grey;
+    margin-top: 1%;
 }
 
-button{
-  border: 1px solid grey;
+button {
+    border: 1px solid grey;
 
 }
 </style>
--- src/components/MyFirstModal.vue
+++ src/components/MyFirstModal.vue
@@ -1,14 +1,15 @@
 <template>
 <div>
-    <button class="text-white rounded bg-pink-500 px-6 py-4 font-sans font-semibold shadow-lg hover:bg-pink-400 outline-none active:outline-none mt-5" @click="toggleModal = true">Open Modal</button>
-    <div class="fixed overflow-x-hidden overflow-y-auto inset-0 flex justify-center items-center z-50" v-if="toggleModal">
-        <div class="absolute inset-0 bg-black opacity-25" @click.self="toggleModal = false"></div>
+    <div class="fixed overflow-x-hidden overflow-y-auto inset-0 flex justify-center items-center z-50" v-if="showModal">
+        <div class="absolute inset-0 bg-black opacity-25" @click.self="$emit('update:showModal', false)"></div>
         <div class="relative mx-auto w-auto max-w-2xl">
             <div class="bg-white rounded shadow-xl w-96 h-56 flex flex-col">
                 <div class="text-sky-500 text-2xl font-bold px-4 py-6">{{ modalTitle }}</div>
                 <span>{{ modalContent }}</span>
                 <div class="mt-4">
                     <slot name="links">
+                        <a href="#" class="text-white rounded bg-green-500 px-4 py-2 font-sans font-semibold shadow-lg hover:bg-green-400 outline-none active:outline-none">Green Button</a>
+                        <a href="#" class="text-white rounded bg-red-500 px-4 py-2 font-sans font-semibold shadow-lg hover:bg-red-400 outline-none active:outline-none">Red Button</a>
                     </slot>
                 </div>
             </div>
@@ -20,7 +21,7 @@
     
 <script>
 export default {
-    name: "MyfirstModal",
+    name: "MyirstModal",
     props: {
         modalTitle: {
             type: String,
@@ -29,12 +30,12 @@ export default {
         modalContent: {
             type: String,
             required: true
+        },
+        showModal: {
+            type: Boolean,
+            required: true,
+            default: false
         }
-    },
-    data() {
-        return {
-            toggleModal: false
-        };
     }
 };
 </script>
