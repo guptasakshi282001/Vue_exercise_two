9573c91e376aaaad892fb73aa6aabf1bb7d0d1d4
Kunal-inno <sakshi_gupta@innotechteam.in>
2023-04-19 17:42:19 +0530

    finalchanges

--- src/components/MyFirstModal.vue
+++ src/components/MyFirstModal.vue
@@ -3,9 +3,9 @@
     <div class="fixed overflow-x-hidden overflow-y-auto inset-0 flex justify-center items-center z-50" v-if="showModal">
         <div class="absolute inset-0 bg-black opacity-25" @click.self="closeModal"></div>
         <div class="relative mx-auto w-auto max-w-2xl">
-            <div class="bg-white rounded shadow-xl w-96 h-56 flex flex-col">
-                <div class="text-sky-500 text-2xl font-bold px-4 py-6" v-bind:class="{'text-red-500': theme === 'sale'}">{{ modalTitle }}</div>
-                <span>{{ modalContent }}</span>
+            <div class="bg-white rounded shadow-xl w-96 h-56 flex flex-col" v-bind:class="{'bg-rose-600': theme === 'sale'}">
+                <div class="text-sky-500 text-2xl font-bold px-4 py-6" v-bind:class="{'text-zinc-50': theme === 'sale'}">{{ modalTitle }}</div>
+                <span v-bind:class="{'text-zinc-50': theme === 'sale'}">{{ modalContent }}</span>
                 <div class="mt-4">
                     <slot name="links">
                     </slot>
