2294505c93ca0d4f1d7d5a186918e289be4ef059
Kunal-inno <sakshi_gupta@innotechteam.in>
2023-04-19 18:55:47 +0530

    exetwochanges

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
 <input :value="text" ref="myInput" />
 <button @click="focusOnInput">Focus</button>
-<button class="text-white rounded bg-pink-500 px-6 py-4 font-sans font-semibold shadow-lg hover:bg-pink-400 outline-none active:outline-none mt-5" @click="toggleModal = !toggleModal">Open Modal</button>
+<button class="text-white rounded bg-pink-500 px-6 py-4 font-sans font-semibold shadow-lg hover:bg-pink-400 outline-none active:outline-none mt-5" @click="showModal = true">Open Modal</button>
 
-<MyFirstModal modalTitle="Sign Up for the GiveAway" modalContent="Grab Your ninja swag for half price" theme="sale" :show-modal="toggleModal" @updateShowModal = "toggleModal = $event">
+<my-first-modal modalTitle="Sign Up for the GiveAway" modalContent="Grab Your ninja swag for half price" theme="sale" :show-modal="showModal" @updateShowModal="showModal = $event">
   <template v-slot:links>
         <a href="#" class="text-white rounded bg-green-500 px-4 py-2 font-sans font-semibold shadow-lg hover:bg-green-400 outline-none active:outline-none">Green Button</a>
         <a href="#" class="text-white rounded bg-red-500 px-4 py-2 font-sans font-semibold shadow-lg hover:bg-red-400 outline-none active:outline-none">Red Button</a>
     </template>
-</MyFirstModal>
+</my-first-modal>
 </template>
 
   
-  
+
 <script>
 import MyFirstModal from './components/MyFirstModal.vue';
 
@@ -27,13 +27,11 @@ export default {
     },
     data() {
         return {
-
-            message: '',
-            toggleModal: false
+            showModal: false
         }
     }
 };
-</script>
+</script> 
 
 <style>
 #app {
