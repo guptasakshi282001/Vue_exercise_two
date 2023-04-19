b53b4495ecf6a20af2ca123578e2b493a6ef66cf
Kunal-inno <sakshi_gupta@innotechteam.in>
2023-04-19 19:18:35 +0530

    finalchangesinexetwo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
 <input :value="text" ref="myInput" />
 <button @click="focusOnInput">Focus</button>
-<button class="text-white rounded bg-pink-500 px-6 py-4 font-sans font-semibold shadow-lg hover:bg-pink-400 outline-none active:outline-none mt-5" @click="showModal = true">Open Modal</button>
+<button class="text-white rounded bg-pink-500 px-6 py-4 font-sans font-semibold shadow-lg hover:bg-pink-400 outline-none active:outline-none mt-5" @click = openModal()>Open Modal</button>
 
-<my-first-modal modalTitle="Sign Up for the GiveAway" modalContent="Grab Your ninja swag for half price" theme="sale" :show-modal="showModal" @updateShowModal="showModal = $event">
+<my-first-modal modalTitle="Sign Up for the GiveAway" modalContent="Grab Your ninja swag for half price" theme="sale" :show-modal="isModalVisible" @updateShowModal="hideModal">
   <template v-slot:links>
         <a href="#" class="text-white rounded bg-green-500 px-4 py-2 font-sans font-semibold shadow-lg hover:bg-green-400 outline-none active:outline-none">Green Button</a>
         <a href="#" class="text-white rounded bg-red-500 px-4 py-2 font-sans font-semibold shadow-lg hover:bg-red-400 outline-none active:outline-none">Red Button</a>
@@ -23,11 +23,18 @@ export default {
     methods: {
         focusOnInput() {
             this.$refs.myInput.focus();
+        },
+        openModal() {
+            this.isModalVisible = true;
+        },
+        hideModal() {
+            this.isModalVisible = false;
         }
     },
     data() {
         return {
-            showModal: false
+            message :"",
+            isModalVisible : false,
         }
     }
 };
